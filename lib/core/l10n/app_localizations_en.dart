da802a1dbfd83b3c3b37c196aafa8298d98f7c8d
Daniil <najdisd2@mail.ru>
2025-06-12 20:29:33 +0300

    feat: create en ru localization

---
+++ lib/core/l10n/app_localizations_en.dart
@@ -0,0 +1,40 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get appBarAccount => 'My account';
+
+  @override
+  String get appBarExpenses => 'Expenses today';
+
+  @override
+  String get appBarIncome => 'Income today';
+
+  @override
+  String get appBarSettings => 'Settings';
+
+  @override
+  String get appBarStats => 'My stats';
+
+  @override
+  String get navBarAccount => 'Account';
+
+  @override
+  String get navBarExpenses => 'Expenses';
+
+  @override
+  String get navBarIncome => 'Income';
+
+  @override
+  String get navBarSettings => 'Settings';
+
+  @override
+  String get navBarStats => 'Stats';
+}
---
+++ lib/core/l10n/app_localizations_ru.dart
@@ -0,0 +1,40 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Russian (`ru`).
+class AppLocalizationsRu extends AppLocalizations {
+  AppLocalizationsRu([String locale = 'ru']) : super(locale);
+
+  @override
+  String get appBarAccount => 'Мой счет';
+
+  @override
+  String get appBarExpenses => 'Расходы сегодня';
+
+  @override
+  String get appBarIncome => 'Доходы сегодня';
+
+  @override
+  String get appBarSettings => 'Настройки';
+
+  @override
+  String get appBarStats => 'Мои статьи';
+
+  @override
+  String get navBarAccount => 'Счет';
+
+  @override
+  String get navBarExpenses => 'Расходы';
+
+  @override
+  String get navBarIncome => 'Доходы';
+
+  @override
+  String get navBarSettings => 'Настройки';
+
+  @override
+  String get navBarStats => 'Статьи';
+}
