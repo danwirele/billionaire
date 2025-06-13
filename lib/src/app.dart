44e4fa029d362d021278cb1aa69165051d02b081
Daniil <najdisd2@mail.ru>
2025-06-13 14:51:29 +0300

    fix: remove useless mixin and convert App widget to stl

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -2,35 +2,14 @@ import 'package:billionaire/core/l10n/app_localizations.dart';
 import 'package:billionaire/src/router/router.dart' show router;
 import 'package:flutter/material.dart';
 
-class App extends StatefulWidget {
+class App extends StatelessWidget {
   const App({super.key});
-
-  @override
-  State<App> createState() => _AppState();
-}
-
-class _AppState extends State<App>
-    with SingleTickerProviderStateMixin {
-  late TabController _tabController;
-
-  @override
-  void initState() {
-    super.initState();
-    _tabController = TabController(length: 5, vsync: this);
-  }
-
-  @override
-  void dispose() {
-    _tabController.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
-      // locale: Locale('en', 'US'),
       locale: Locale('ru', 'RU'),
       routerConfig: router,
       theme: ThemeData.light(useMaterial3: true),
